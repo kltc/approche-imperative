f6780864b7ca2db73e2598c5ede3ced9114219c2
Kevin LE <kevin.letc13@gmail.com>
2024-04-04 14:24:36 +0200

    premier commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,25 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		byte b = 12;
+		short s = -15325;
+		int i = 1250326;
+		long l = 10000L;
+		float f = 1.8F;
+		double d = 1.8;
+		char c = 'a';
+		String chaine = "Salut";
+		boolean bool = true;
+		
+		System.out.println(chaine);
+		
+		String randomString = "Voici le résultat d'un calcul : 1+5=6";
+		
+		System.out.println(randomString);
+		
+
+	}
+
+}
